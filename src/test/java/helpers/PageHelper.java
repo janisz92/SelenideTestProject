5a3882ae78055019e6a93d9d2b53c97bf9b68bbe
bjnisze@gmail.com <Polska123456!>
2020-04-25 20:49:28 +0200

    add logger

--- src/test/java/helpers/PageHelper.java
+++ src/test/java/helpers/PageHelper.java
@@ -3,14 +3,14 @@ package helpers;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import pages.CreateAccountPage;
+
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class PageHelper {
 
     private WebDriverWait wait = new WebDriverWait(getWebDriver(), 5L);
-    protected Logger log = Logger.getLogger(CreateAccountPage.class);
+    protected Logger log = Logger.getLogger(PageHelper.class);
 
     public void waitABit(int millisecondToWait) {
         try {
